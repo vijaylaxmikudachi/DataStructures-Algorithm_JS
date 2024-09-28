4d29503c2976187ef91daac03396c4514c823c52
Vijaylaxmi <vijaylaxmikudachi80@gmail.com>
2024-09-28 11:21:48 +0530

    [vijaylaxmi]Added:linked list

--- Linked_list/insert.js
+++ Linked_list/insert.js
@@ -18,13 +18,12 @@ appendNode(nodeData){
 
 }
 traversing(){
-    let count = 0;
     let currentNode = this.head;
     let printlist = '';
-    while(count<this.size){
+    while(currentNode){
         printlist+=currentNode.value+'->';
         currentNode = currentNode.next;
-        count++;
+
     }
     console.log(printlist+'null');
 }
@@ -59,16 +58,50 @@ insertNode(index,Value){
     };
     this.size++;
 }
+search(ele){
+    let currentNode = this.head;
+    var isp = false;
+    while(currentNode){
+        if(currentNode.value === ele){
+            isp = true;
+        }
+        currentNode = currentNode.next;
+    }
+    return isp;
+}
+boubleS(){
+    let swap ;
+    let currentNode = this.head;
+    do{
+         swap = false;
+        while(currentNode!==null && currentNode.next!==null){
+            if(currentNode.value>currentNode.next.value){
+                let temp = currentNode.data;
+                currentNode.value = currentNode.next.value;
+                currentNode.next.value = temp;
+                swap = true;
+            }
+            currentNode = currentNode.next;
+        }
 
+    }while(swap);
+}
 }
 let list = new List(100);
-list.appendNode(200);
 list.appendNode(300);
+list.appendNode(200);
+
 list.appendNode(400);
 list.traversing();
 
-list.deleteNode(2);
+//list.deleteNode(2);
 list.traversing();
 
 list.insertNode(2,1000);
 list.traversing();
+list.boubleS();
+list.traversing();
+
+
+console.log("Searching for 300:", list.search(300));  // true
+console.log("Searching for 50:", list.search(50));  // false
