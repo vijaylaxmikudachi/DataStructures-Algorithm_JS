eb09dcce2edc9f7ed5bc351c243de3514229b633
Vijaylaxmi <vijaylaxmikudachi80@gmail.com>
2024-09-28 12:05:26 +0530

    [vijaylaxmi]Added:Sorting functions

---
+++ Sorting/insertion.js
@@ -0,0 +1,20 @@
+function selectionSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        // Swap the found minimum element with the first element
+        if (minIndex !== i) {
+            let temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+    return arr;
+}
+let arr = [64, 34, 25, 12, 22, 11, 90];
+console.log("Insertion Sort: ", insertionSort([...arr]));
